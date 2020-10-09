8d3cc3f9ca3bbfedc6420442a948f6ede080a32b
xc171 <xc171@students.waikato.ac.nz>
2020-10-10 05:35:34 +1300

    First sprint. Small update of bouncer.

--- app/src/main/java/com/example/assignmentseven/Bouncer.java
+++ app/src/main/java/com/example/assignmentseven/Bouncer.java
@@ -3,11 +3,14 @@ package com.example.assignmentseven;
 public class Bouncer extends Circle {
 
     private float xAcceleration;
+    private float h,k;
 
     public Bouncer(float xB,float yB, int c, float width){
         super();
         x = xB;
         y = yB;
+        h = xB;
+        k = yB;
         color = c;
         radius = 200;
         boundary = width;
@@ -17,16 +20,23 @@ public class Bouncer extends Circle {
         float interval = 0.4f;
         float xSpeed = xVelocity;
         float acceleration;
-        if(x>(boundary*2/3))
-            acceleration = xAcceleration + ACCELERATION;
-        else if(x<(boundary/3))
-            acceleration = xAcceleration - ACCELERATION;
+        if(x>(boundary*4/7)){
+            acceleration = xAcceleration - 2*ACCELERATION;
+            if(x>(boundary*5/7))
+                acceleration -= 2*ACCELERATION;
+        }
+        else if(x<(boundary*3/7)){
+            acceleration = xAcceleration + 2*ACCELERATION;
+            if(x<(boundary*2/7))
+                acceleration += 2*ACCELERATION;
+        }
         else
             acceleration = xAcceleration;
         float xDistance = (xSpeed + acceleration*interval/2)*interval;
-        xVelocity -= acceleration*interval;
-        //float xDistance = (xSpeed + xAcceleration*interval/2)*interval;
+        xVelocity += acceleration*interval;
+
         x += xDistance;
+        y = k - (x-h)*(x-h)/2000;
         if(x<=radius){
             x=radius;
             xVelocity = -xVelocity/3;
@@ -38,7 +48,8 @@ public class Bouncer extends Circle {
     }
 
     public void update(float xAcc){
-        xAcceleration = xAcc;
+
+        xAcceleration = -xAcc;
     }
 
 
